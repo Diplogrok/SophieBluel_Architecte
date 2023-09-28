5e9f7a6b5cdaeb082080933367cea978c744982b
Alexia Stephan <alexia0stephan@gmail.com>
2023-09-28 18:20:56 +0200

    ajout appel api categories

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -1,9 +1,9 @@
+const apiWork = "http://localhost:5678/api/works";
 const container = document.getElementById("gallery");
 const filterButtons = document.getElementById("filter").children;
-const url = "http://localhost:5678/api/works";
 
-const getWorks = () => {
-  fetch(url)
+const getWorks = async () => {
+  await fetch(apiWork)
     .then(function (response) {
       return response.json();
     })
@@ -17,32 +17,36 @@ const getWorks = () => {
         <figcaption>${data[work].title}</figcaption>
         </figure>`;
       }
-
-      filterButtons[0].addEventListener("click", function () {
-        console.log("Tous");
-
-        /*let All = fetch(url).categoryId.filter(function (value) {
-          if ((value === 1, 2, 3)) {
-            return true;
-          }
-        });*/
-      });
-
-      filterButtons[1].addEventListener("click", function () {
-        console.log("Objets");
-
-        let objects = url.filter((category) => category.categoryId === 1);
-        objects.forEach((category) => console.log(category.title));
-      });
-
-      filterButtons[2].addEventListener("click", function () {
-        console.log("Appartements");
-      });
-
-      filterButtons[3].addEventListener("click", function () {
-        console.log("Hôtel & restaurants");
-      });
     });
 };
 
 getWorks();
+
+const apiCategory = "http://localhost:5678/api/categories";
+
+//array.prototype.filter()//
+
+filterButtons[0].addEventListener("click", function () {
+  console.log("Tous");
+
+  /*let All = fetch(url).categoryId.filter(function (value) {
+          if ((value === 1, 2, 3)) {
+            return true;
+          }
+        });*/
+});
+
+filterButtons[1].addEventListener("click", function () {
+  console.log("Objets");
+
+  let objects = url.filter((category) => category.categoryId === 1);
+  objects.forEach((category) => console.log(category.title));
+});
+
+filterButtons[2].addEventListener("click", function () {
+  console.log("Appartements");
+});
+
+filterButtons[3].addEventListener("click", function () {
+  console.log("Hôtel & restaurants");
+});
