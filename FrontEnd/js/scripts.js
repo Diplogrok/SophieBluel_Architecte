d1e913834ec5b4f7b6f666545c11c924ce5a67a0
Alexia Stephan <alexia0stephan@gmail.com>
2023-10-12 19:18:17 +0200

    ajout fonction filtrage des categories

--- FrontEnd/js/scripts.js
+++ FrontEnd/js/scripts.js
@@ -1,104 +1,57 @@
-const apiWork = "http://localhost:5678/api/works";
-const apiCategory = "http://localhost:5678/api/categories";
 const container = document.getElementById("gallery");
 const buttonFilter = document.getElementById("filter");
 const buttonFilterChildren = buttonFilter.children;
-let dataSaved = [];
-let dataSavedC = [];
-
-const getWork = async () => {
-  await fetch(apiWork)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      dataSaved = data;
-
-      for (work in data) {
-        container.innerHTML += `
-        <figure>
-        <img src="${data[work].imageUrl}">
-        <figcaption>${data[work].title}</figcaption>
-        </figure>`;
-      }
-    });
-};
-
-getWork();
-
 let all = `
 <button class="active">Tous</button>
 `;
 buttonFilter.innerHTML = all;
 
-const getCategory = async () => {
-  await fetch(apiCategory)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      dataSavedC = data;
-
-      for (category in data) {
-        buttonFilter.innerHTML += `
-        <button>${data[category].name}</button>
+(async () => {
+  const answerW = await fetch("http://localhost:5678/api/works");
+  const work = await answerW.json();
+  function getWork(work) {
+    for (let i = 0; i < work.length; i++) {
+      container.innerHTML += `
+        <figure>
+        <img src="${work[i].imageUrl}">
+        <figcaption>${work[i].title}</figcaption>
+        </figure>`;
+    }
+  }
+  const answerC = await fetch("http://localhost:5678/api/categories");
+  const category = await answerC.json();
+  for (let j = 0; j < category.length; j++) {
+    buttonFilter.innerHTML += `
+        <button>${category[j].name}</button>
         `;
-      }
-    });
+  }
+  getWork(work);
   buttonFilterChildren[0].addEventListener("click", function () {
-    let objects = dataSaved.filter((category) => category.categoryId <= 3);
-
-    objects.forEach((category) => console.log(category.title));
+    const allFiltered = work.filter(function (objects) {
+      return objects.categoryId <= 3;
+    });
+    document.getElementById("gallery").innerHTML = "";
+    getWork(allFiltered);
   });
-
   buttonFilterChildren[1].addEventListener("click", function () {
-    let objects = dataSaved.filter((category) => category.categoryId === 1);
-
-    objects.forEach((category) => console.log(category.title));
+    const objectsFiltered = work.filter(function (objects) {
+      return objects.categoryId === 1;
+    });
+    document.getElementById("gallery").innerHTML = "";
+    getWork(objectsFiltered);
   });
   buttonFilterChildren[2].addEventListener("click", function () {
-    let objects = dataSaved.filter((category) => category.categoryId === 2);
-
-    objects.forEach((category) => console.log(category.title));
+    const appartmentFiltered = work.filter(function (objects) {
+      return objects.categoryId === 2;
+    });
+    document.getElementById("gallery").innerHTML = "";
+    getWork(appartmentFiltered);
   });
   buttonFilterChildren[3].addEventListener("click", function () {
-    let objects = dataSaved.filter((category) => category.categoryId === 3);
-
-    objects.forEach((category) => console.log(category.title));
+    const hotelFiltered = work.filter(function (objects) {
+      return objects.categoryId === 3;
+    });
+    document.getElementById("gallery").innerHTML = "";
+    getWork(hotelFiltered);
   });
-};
-
-getCategory();
-
-/*
-buttonFilterChildren.forEach((color) => {
-  color.addEventListener("click", () => {
-    color.classList.add("active");
-  });
-});
-/*
-function nextCat() {
-          buttonFilterChildren[i].classList.remove("active");
-          if (i < buttonFilterChildren.length - i) {
-            i++;
-          } else {
-            i = 0;
-          }
-          buttonFilterChildren[i].classList.add("active");
-        }
-        buttonFilterChildren.item(i).addEventListener("click", nextCat);
-
-
-/*for (let filter of buttonFilterChildren) {
-        filter.addEventListener("click", function () {
-          let tag = this.id.name;
-          console.log(tag);
-        });
-      }*/
-
-/* filterButtons[1].addEventListener("click", function () {
-  let objects = dataSaved.filter(
-    (category) => category.category.name === "Objets"
-  );
-  objects.forEach((category) => console.log(category.title));
-}); */
+})();
