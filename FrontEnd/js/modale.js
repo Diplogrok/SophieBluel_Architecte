d2e4c4beac049c5a12401b14b1d14a11b4b025ce
Alexia Stephan <alexia0stephan@gmail.com>
2023-10-27 16:15:01 +0200

    affichage au click de la fenêtre modale

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,9 +1,8 @@
 const openModal = function (e) {
   e.preventDefault();
   const target = document.querySelector(e.target.getAttribute("href"));
-  console.log(target);
   target.style.display = null;
-  target.remove("aria-hidden");
+  target.removeAttribute("aria-hidden");
   target.setAttribute("aria-modal", true);
 };
 document.querySelectorAll(".js-modal").forEach((a) => {
