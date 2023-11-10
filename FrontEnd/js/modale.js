66d7b0b4c6521ce8ce09447498fbe88c0c6cfebb
Alexia Stephan <alexia0stephan@gmail.com>
2023-11-10 12:35:57 +0100

    Upload image et previex fonctionnel

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -69,3 +69,25 @@ window.addEventListener("keydown", function (e) {
   }
 });
 
+const selectImage = document.querySelector(".img-selected");
+const inputFile = document.querySelector("#file");
+const imgArea = document.querySelector(".img-area");
+
+selectImage.addEventListener("click", function () {
+  inputFile.click();
+});
+
+inputFile.addEventListener("change", function () {
+  const image = this.files[0];
+  console.log(image);
+  const reader = new FileReader();
+  reader.onload = () => {
+    const imgUrl = reader.result;
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    imgArea.appendChild(img);
+    selectImage.classList.add("background");
+    selectImage.classList.remove("img-selected");
+  };
+  reader.readAsDataURL(image);
+});
